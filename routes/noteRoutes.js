5c072c029bc0baa9e05ac0a537cda38be88bc561
Paul Pearce <pearce.o.paul2020@gmail.com>
2021-09-24 09:24:29 -0700

    Added database compatability

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,23 +1,42 @@
 const router = require('express').Router()
 const { join } = require('path')
+const fs = require('fs')
 let notes = require('../db/db.json')
+const { json } = require('express')
 
 router.get('/notes', (req,res) => {
   res.sendFile(join(__dirname, '../public/notes.html'))
 })
 
 router.get('/api/notes', (req, res) => {
-  res.json(notes)
+  fs.readFile('db/db.json', (err,data) =>{
+  	if(err){console.log(err)}
+  	res.json(JSON.parse(data))
+	})
 })
-//Add API
+
 router.post('/api/notes', (req, res) => {
   notes.push(req.body)
+  fs.writeFile('db/db.json', JSON.stringify(notes), err =>{
+  	if(err){console.log(err)}
+	})
   res.sendStatus(200)
 })
-//Add api
+
 router.delete('/api/notes/:id', (req, res) => {
   let id = req.params.id
-  notes = notes.filter(note => note.text !== id)
+  console.log(id)
+  fs.readFile('db/db.json',  (err,data) =>{
+    if(err){console.log(err)}
+    console.log()
+    data = JSON.parse(data).filter(note => note.text !== id)
+    fs.writeFile('db/db.json', JSON.stringify(data), err =>{
+  	  if(err){console.log(err)}
+	  })
+
+	})
+
+  
   res.sendStatus(200)
 })
 //backup url
